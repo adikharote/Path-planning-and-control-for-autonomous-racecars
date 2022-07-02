a14edc065ea67a35c62fecf695e3e687900af7ba
AhaanKotian <97961155+AhaanKotian@users.noreply.github.com>
2022-07-02 19:46:03 +0530

    Update mainPD.py

--- controls/mainPD.py
+++ controls/mainPD.py
@@ -2,18 +2,12 @@ import rospy
 import time
 from PD import PD
 
-t1 = time.time()
-
 def main():
     rospy.init_node('Control_node')
     controller = PD()
     while not rospy.is_shutdown():
         if controller.current_state != None and controller.velocity != None:
-            t2 = time.time()
-            if t2 - t1 == 5:
-                controller.runPD(0)
-            else:
-                controller.runPD(2)
+            controller.runPD()
 
 if __name__ == '__main__':
     try:

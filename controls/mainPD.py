bec3cb1ef063afe2c038b8c3dd7b87795bc5d93c
AhaanKotian <97961155+AhaanKotian@users.noreply.github.com>
2022-07-02 14:48:19 +0530

    Update mainPD.py

--- controls/mainPD.py
+++ controls/mainPD.py
@@ -1,17 +1,19 @@
 import rospy
-from PD import PD
+import time
+from PID import PD
 
+t1 = time.time()
 
 def main():
     rospy.init_node('Control_node')
     controller = PD()
     while not rospy.is_shutdown():
-        print("Juniors kuch kaam nahi kar rahe")
-        if controller.current_state != None and controller.velocity != None:
-            # print("Agar yeh run hua, sablog ko mai nikal raha hai")
-            controller.runPD()
-        else:
-            print(controller.current_state)
+        if controller.current_state_ != None and controller.x_pos != None and controller.velocity != None:
+            t2 = time.time()
+            if t2 - t1 == 5:
+                controller.runPD(0)
+            else:
+                controller.runPD(2)
 
 if __name__ == '__main__':
     try:
